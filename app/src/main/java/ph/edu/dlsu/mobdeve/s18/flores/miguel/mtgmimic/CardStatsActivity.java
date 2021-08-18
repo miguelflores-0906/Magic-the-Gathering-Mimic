d74d7fc48f73eb01c741942856fec526dfd498ad
MatthewSingson <80302716+MatthewSingson@users.noreply.github.com>
2021-08-18 21:54:52 +0800

    added more layouts

---
+++ app/src/main/java/ph/edu/dlsu/mobdeve/s18/flores/miguel/mtgmimic/CardStatsActivity.java
@@ -0,0 +1,14 @@
+package ph.edu.dlsu.mobdeve.s18.flores.miguel.mtgmimic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CardStatsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_stats);
+    }
+}
