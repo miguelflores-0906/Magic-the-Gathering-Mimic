b2e8a4296af4e10813e2af4435fa9563b7867bc3
miguelflores-0906 <miggy_4th@yahoo.com>
2021-08-19 09:54:33 +0800

    added the cardview for masterlist

--- app/src/main/java/ph/edu/dlsu/mobdeve/s18/flores/miguel/mtgmimic/MainActivity.java
+++ app/src/main/java/ph/edu/dlsu/mobdeve/s18/flores/miguel/mtgmimic/MainActivity.java
@@ -3,6 +3,8 @@ package ph.edu.dlsu.mobdeve.s18.flores.miguel.mtgmimic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.btn_test);
+
+//        this is just a test for when I figure out how to use the json file later on kek
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+
+            }
+        });
     }
 }
