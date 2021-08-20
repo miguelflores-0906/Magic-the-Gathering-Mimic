6587a8d9ddf9fb0cb433f4a5e36cd8f4c71b0874
miguelflores-0906 <miggy_4th@yahoo.com>
2021-08-20 19:00:26 +0800

    Made Menu Bar and Fragments
    We might need to update the front-end, I'll do that in a while

--- app/src/main/java/ph/edu/dlsu/mobdeve/s18/flores/miguel/mtgmimic/MainActivity.java
+++ app/src/main/java/ph/edu/dlsu/mobdeve/s18/flores/miguel/mtgmimic/MainActivity.java
@@ -1,26 +1,71 @@
 package ph.edu.dlsu.mobdeve.s18.flores.miguel.mtgmimic;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button = findViewById(R.id.btn_test);
+        Toolbar toolbar = findViewById(R.id.tb_master);
+        setSupportActionBar(toolbar);
 
-//        this is just a test for when I figure out how to use the json file later on kek
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Code here executes on main thread after user presses button
+        drawer = findViewById(R.id.drawer_layout);
 
-            }
-        });
+        NavigationView navigationView = findViewById(R.id.nav_master_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,toolbar,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new TestFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_master);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.nav_master:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new TestFragment()).commit();
+                break;
+            case R.id.nav_inv:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new TestFragment2()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
---
+++ app/src/main/java/ph/edu/dlsu/mobdeve/s18/flores/miguel/mtgmimic/TestFragment2.java
@@ -0,0 +1,18 @@
+package ph.edu.dlsu.mobdeve.s18.flores.miguel.mtgmimic;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class TestFragment2 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.test_fragment_2, container, false);
+    }
+}
