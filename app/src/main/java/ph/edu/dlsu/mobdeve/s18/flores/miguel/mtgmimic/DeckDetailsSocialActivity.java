1f7a69fc7c83c84c7d3af84322ec71dcf8a36c0f
miguelflores-0906 <miggy_4th@yahoo.com>
2021-09-16 17:03:09 +0800

    MasterCardList now shows all standard 2022 cards

--- app/src/main/java/ph/edu/dlsu/mobdeve/s18/flores/miguel/mtgmimic/DeckDetailsSocialActivity.java
+++ app/src/main/java/ph/edu/dlsu/mobdeve/s18/flores/miguel/mtgmimic/DeckDetailsSocialActivity.java
@@ -11,13 +11,17 @@ import android.widget.TextView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import io.magicthegathering.javasdk.api.SetAPI;
+import io.magicthegathering.javasdk.resource.Card;
+import io.magicthegathering.javasdk.resource.MtgSet;
 import ph.edu.dlsu.mobdeve.s18.flores.miguel.mtgmimic.databinding.ActivityDeckDetailsBinding;
 
 public class DeckDetailsSocialActivity extends AppCompatActivity implements MasterCardlistAdapter.ItemClickListener {
 
     private DeckDetailsSocialActivity binding;
-    private ArrayList<Card> cardArrayList;
+    private ArrayList<io.magicthegathering.javasdk.resource.Card> cardArrayList;
     private MasterCardlistAdapter adapter;
 
     @Override
@@ -45,7 +49,40 @@ public class DeckDetailsSocialActivity extends AppCompatActivity implements Mast
 
         // Recycler View
 
-        cardArrayList = CustomDataHelper.loadCards();
+//        cardArrayList = CustomDataHelper.loadCards();
+
+        // call API to get all standard 2022 cards
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // Adventures in the Forgotten Realms
+                MtgSet afr = SetAPI.getSet("AFR");
+                List<io.magicthegathering.javasdk.resource.Card> afrCards = afr.getCards();
+
+                // Strixhaven
+                MtgSet stx = SetAPI.getSet("STX");
+                List<io.magicthegathering.javasdk.resource.Card> stxCards = stx.getCards();
+
+                // Kaldheim
+                MtgSet khm = SetAPI.getSet("KHM");
+                List<io.magicthegathering.javasdk.resource.Card> khmCards = khm.getCards();
+
+                // Zendikar Rising
+                MtgSet znr = SetAPI.getSet("ZNR");
+                List<io.magicthegathering.javasdk.resource.Card> znrCards = znr.getCards();
+
+                // Arena Base Set
+                MtgSet anb = SetAPI.getSet("ANB");
+                List<io.magicthegathering.javasdk.resource.Card> anbCards = anb.getCards();
+
+                cardArrayList.addAll(afrCards);
+                cardArrayList.addAll(stxCards);
+                cardArrayList.addAll(khmCards);
+                cardArrayList.addAll(znrCards);
+                cardArrayList.addAll(anbCards);
+            }
+        }).start();
+
         adapter = new MasterCardlistAdapter(cardArrayList, this);
 
         RecyclerView recyclerView = findViewById(R.id.rv_sdeckdetailscards);
@@ -64,7 +101,7 @@ public class DeckDetailsSocialActivity extends AppCompatActivity implements Mast
     @Override
     public void onItemClick(Card card) {
         Intent intent = new Intent(getApplicationContext(), CardDetailsActivity.class);
-        intent.putExtra("cardName", card.getCardName());
+        intent.putExtra("cardName", card.getName());
         intent.putExtra("cardExp", card.getSet());
         intent.putExtra("cardType", card.getType());
         startActivity(intent);
