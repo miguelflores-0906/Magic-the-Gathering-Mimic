c28edc421f36ca7981a249c9a556b93c8b0cc423
MatthewSingson <80302716+MatthewSingson@users.noreply.github.com>
2021-09-19 18:28:20 +0800

    minor change
    - cards writing now uses email instead of display name (just in case of duplicate usernames since no error checking)

--- app/src/main/java/ph/edu/dlsu/mobdeve/s18/flores/miguel/mtgmimic/DeckBuilderFragment.java
+++ app/src/main/java/ph/edu/dlsu/mobdeve/s18/flores/miguel/mtgmimic/DeckBuilderFragment.java
@@ -81,7 +81,7 @@ public class DeckBuilderFragment extends Fragment {
 
             // TODO: db stuff
             Deck deck = new Deck();
-            deck.setUsername(fAuth.getCurrentUser().getDisplayName());
+            deck.setUsername(fAuth.getCurrentUser().getEmail());
             deck.setDeckname(et_deck_name.getText().toString());
             deck.setCards(sb.toString());
             deckDBDAO.addDeck(deck);
